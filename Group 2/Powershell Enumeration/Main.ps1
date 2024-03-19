ef09c1b3be11d2d5cc4e14540bbda3cfc0a50d80
Durojaye Olusegun <91230297+GeoSegun@users.noreply.github.com>
2024-03-19 14:10:31 +0100

    Powershell Enumeration submission (#6)
    * keveros submission
    
    * i changed the folder name
    
    * i changed the folder name
    
    * powershell enumeration added
    
    * powershell enumeration added with some corrections

---
+++ Group 2/Powershell Enumeration/Main.ps1
@@ -0,0 +1,48 @@
+﻿# Just do it
+
+# Import my core script
+
+import-module C:\PSFolder\Core.ps1
+
+$directoryPath = "C:\Program Files"
+Get-DirectoryACL -Path $directoryPath
+$directoryPath
+
+
+$hostname = Get-Hostname
+$hostname
+
+
+$securityGroups = Get-SecurityGroups
+$securityGroups
+
+
+$processes = Get-Processes
+$processes
+
+
+$ipAddresses = Get-IPAddresses
+$ipAddresses
+
+
+$domains = Get-Domains
+$domains
+
+
+$registry = Get-Registry
+$registry
+
+
+$services = Get-Services
+$services
+
+
+$scheduledTasks = Get-ScheduledTasks
+$scheduledTasks
+
+
+$autoStartProcesses = Get-AutoStartProcesses
+$autoStartProcesses
+
+# Terminate the script
+exit
